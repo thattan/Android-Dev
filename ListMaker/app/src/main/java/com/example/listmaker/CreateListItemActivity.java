638eb008bb867e036eed5f85fa4e88cf1d1336f3
Tyler Hattan <th633542@southeast.edu>
2021-03-11 11:54:20 -0600

    Part 1 complete

---
+++ ListMaker/app/src/main/java/com/example/listmaker/CreateListItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.listmaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateListItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_list_item);
+    }
+}
